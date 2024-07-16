de3aabfd460ed9fff256dd140c424d7aa09f1932
anandspaces <anandvaibhaw000@gmail.com>
2024-07-17 03:59:02 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import Feed from '../components/Feed'
+import Leftbar from '../components/Leftbar'
+import Rightbar from '../components/Rightbar'
+import Topbar from '../components/Topbar'
+import './home.css'
+
+export default function Home() {
+  return (
+    <>
+      <Topbar />
+      <div className='./homeContainer'>
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+      </div>
+
+    </>
+  )
+}
