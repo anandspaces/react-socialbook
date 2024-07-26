fbd6ace5082108c2b853b146098934028537718f
anandspaces <anandvaibhaw000@gmail.com>
2024-07-26 12:45:11 +0530

    added like functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,19 @@
-import { MoreVert } from '@mui/icons-material'
-import './post.css'
-import { Users } from '../dummyData'
+import { MoreVert } from '@mui/icons-material';
+import './post.css';
+import { Users } from '../dummyData';
+import { useState } from 'react';
 
 export default function Post({post}) {
   // const user = Users.filter(u=>u.id===1);
   // console.log(user);
   // console.log(user[0].username);
+  const [like,setLike] = useState(post.like);
+  const[isLiked,setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like-1 : like+1);
+    setIsLiked(!isLiked);
+  }
 
   return (
     <div className='post'>
@@ -31,9 +39,9 @@ export default function Post({post}) {
         </div>
         <div className='postBottom'>
           <div className='postBottomLeft'>
-            <img className='likeIcon' src='/assets/like.png' alt=''/>
-            <img className='likeIcon' src='/assets/heart.png' alt='' />
-            <span className='postLikeCounter'>{post.like} people liked it</span>
+            <img className='likeIcon' src='/assets/like.png' onClick={likeHandler} alt=''/>
+            <img className='likeIcon' src='/assets/heart.png' onClick={likeHandler} alt='' />
+            <span className='postLikeCounter'>{like} people liked it</span>
           </div>
           <div className='postBottomRight'>
             <span className='postCommentText'>{post.comment} comments</span>
